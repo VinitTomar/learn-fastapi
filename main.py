cf788f5ca3d313c08e30b68af5e4197271b916b9
Vinit Tomar <vinit.tomar@aircall.io>
2026-06-21 21:51:16 +0530

    Query param with validation

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from enum import Enum
+from typing import Annotated
 
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Query
+from pydantic import AfterValidator, BaseModel
 
 app = FastAPI()
 
@@ -17,6 +18,12 @@ class Item(BaseModel):
     is_offer: bool | None = None
 
 
+def check_valid_id(id: str):
+    if not id.startswith(('isbn-', 'imdb-')):
+        raise ValueError("Invalid ID format, must start with ISBN- or IMDB-")
+    return id
+
+
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
@@ -29,9 +36,19 @@ async def read_items(skip: int = 0, limit: int = 10):
 async def create_item(item: Item):
     return item
 
-
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None, short: bool = False):
+def read_item(
+    item_id: int,
+    # Not a query param. Only types 
+    # such as int, str, bool etc. will be considered as query param 
+    # without Annotated
+    t: list[str],
+    uuid: Annotated[str | None, Query(max_length=50), AfterValidator(check_valid_id)] = None,
+    s: Annotated[list[str], Query(alias="super")] = ['foo', 'bar'], 
+    q: str | None = Query(default=None, max_length=50),
+    r: Annotated[str | None, Query(max_length=50)] = None,
+    short: bool = False
+  ):
     item = {"item_id": item_id}
 
     if q:
